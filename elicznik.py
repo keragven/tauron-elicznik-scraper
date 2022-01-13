f65d733928316ed1ac12534e021fe718a413e2ae
Filip Kaszuba <fkaszuba@gmail.com>
2022-01-13 07:37:37 +0100

    added methods

--- elicznik.py
+++ elicznik.py
@@ -7,6 +7,12 @@ from urllib3 import poolmanager
 import datetime
 import json
 
+ONE_DAY = datetime.timedelta(days=1)
+TODAY = datetime.date.today()
+
+
+def dmy2date(str_):
+    return datetime.date(int(str_.split('.')[2]), int(str_.split('.')[1]), int(str_.split('.')[0]))
 
 class TLSAdapter(adapters.HTTPAdapter):
 
@@ -23,47 +29,70 @@ class TLSAdapter(adapters.HTTPAdapter):
 
 
 class Elicznik:
-	
-	url = 'https://logowanie.tauron-dystrybucja.pl/login'
-	charturl = 'https://elicznik.tauron-dystrybucja.pl/index/charts'
-	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0'} 
-	service = 'https://elicznik.tauron-dystrybucja.pl'
-	
-	def __init__(self, credentials_dict):
-		self.username = credentials_dict['username']
-		self.password = credentials_dict['password']
-		self.meter_id = int(credentials_dict['meter_id'])
-		
-	def get_consumption(self, n_days=1):
-		payload = { 
+    
+    url = 'https://logowanie.tauron-dystrybucja.pl/login'
+    charturl = 'https://elicznik.tauron-dystrybucja.pl/index/charts'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0'} 
+    service = 'https://elicznik.tauron-dystrybucja.pl'
+    
+    def __init__(self, credentials_dict):
+        self.username = credentials_dict['username']
+        self.password = credentials_dict['password']
+        self.meter_id = int(credentials_dict['meter_id'])
+        
+    def get_daily_raw(self, n_days=1):
+        payload = { 
                 'username': self.username,
                 'password': self.password,
                 'service': self.service
-		}
-		
-		session = requests.session()
-		session.mount('https://', TLSAdapter())
+        }
+        
+        session = requests.session()
+        session.mount('https://', TLSAdapter())
 
-		p = session.request("POST", self.url, data=payload, headers=self.headers)
-		p = session.request("POST", self.url, data=payload, headers=self.headers)
+        p = session.request("POST", self.url, data=payload, headers=self.headers)
+        p = session.request("POST", self.url, data=payload, headers=self.headers)
 
-		chart = {
-					#change timedelta to get data from another days (1 for yesterday)
-						"dane[chartDay]": (datetime.datetime.now() - datetime.timedelta(n_days)).strftime('%d.%m.%Y'),
-						"dane[paramType]": "day",
-						"dane[smartNr]": self.meter_id,
-					#comment if don't want generated energy data in JSON output:
-						"dane[checkOZE]": "on"
-		}
+        chart = {
+                    #change timedelta to get data from another days (1 for yesterday)
+                        "dane[chartDay]": (datetime.datetime.now() - datetime.timedelta(n_days)).strftime('%d.%m.%Y'),
+                        "dane[paramType]": "day",
+                        "dane[smartNr]": self.meter_id,
+                    #comment if don't want generated energy data in JSON output:
+                        "dane[checkOZE]": "on"
+        }
 
-		r = session.request("POST", self.charturl, data=chart, headers=self.headers)
-		return r.text
+        r = session.request("POST", self.charturl, data=chart, headers=self.headers)
+        return r.json()
 
+    def pick_daily_stats(self, stats_dump):
+        pobr_data = dmy2date(stats_dump['name']['chart'][-10:])
+        prod_data = dmy2date(stats_dump['name']['OZE'][-10:])
+        pobr_kwh = stats_dump['sum']
+        prod_kwh = stats_dump['OZEValue']
+        is_full = stats_dump['isFull']
+        if pobr_data == prod_data:
+            return (pobr_data, pobr_kwh, prod_kwh, is_full)
+        else:
+            raise IOError('Daty produkcji i poboru sa rozne.')
+
+    def get_yesterday_info(self):
+        daily_data_raw = self.get_daily_raw()
+        daily_data = self.pick_daily_stats(daily_data_raw)
+        if daily_data[0] + ONE_DAY == TODAY:
+            if daily_data[2] == True:
+                return daily_data
+            else:
+                raise IOError('Dane z wczoraj sa niekompletne.')
+        else:
+            raise IOError('Brak danych z wczoraj.')
+        
 def main():
     with open('credentials.json', 'r') as cred:
         credentials = json.load(cred)
     licznik = Elicznik(credentials)
-    txt = licznik.get_consumption(n_days=2)
+    txt = licznik.get_yesterday_info()
+    
     print(txt)
 
 
